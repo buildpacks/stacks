452e8b62f70d7bdc86500b5f78e6db76b8f630b6
Danny Joyce <djoyce@pivotal.io>
2019-05-03 10:12:05 -0400

    Adds script to create stack images
    Signed-off-by: Danny Joyce <djoyce@pivotal.io>
    Signed-off-by: Emily Casey <ecasey@pivotal.io>

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# stacks
+# Stacks
+
+## Development
+
+To create bionic stack images run:
+```
+./scipts/create-stack.sh [ -v <version>]
+```
+This will generate `packs/build:<version>` and `packs/run:<version>` images
+
+To use this stack see the `pack create-builder` [documentation](https://github.com/buildpack/pack/blob/master/README.md#working-with-builders-using-create-builder)
---
+++ scripts/create-stack.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+usage() { echo "Usage: $0 [-v <image version>]" 1>&2; exit 1; }
+version=dev
+
+while getopts "v" o; do
+  case "${o}" in
+    v)
+      version=${OPTARG}
+      [[ -n ${version} ]] || usage
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+
+docker pull ubunutu:bionic
+
+scripts_dir=$(cd $(dirname $0) && pwd)
+dir=${scripts_dir}/../bionic
+
+base_image=packs/base:${version}
+run_image=packs/run:${version}
+build_image=packs/build:${version}
+docker build -t "${base_image}" "$dir/base"
+docker build --build-arg "base=${base_image}" -t "${build_image}"  "$dir/build"
+docker build --build-arg "base=${base_image}" -t "${run_image}" "$dir/run"
+
+echo "To publish these images:"
+for image in "${base_image}" "${run_image}" "${build_image}"; do
+  echo "  docker push ${image}"
+done
